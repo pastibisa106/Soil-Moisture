0fd3aeb9e50fe309cc4f04a257364b04721cd3b6
pastibisa106 <35872007+pastibisa106@users.noreply.github.com>
2025-05-07 16:09:10 +0700

    Add files via upload

---
+++ Kelembaban_tanah.ino
@@ -0,0 +1,23 @@
+int pin = A0;
+int kelembaban;
+int pompa = 13;
+void setup() {
+//  put your setup code here, to run once:
+pinMode (pin, INPUT);
+pinMode(pompa, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  //put your main code here, to run repeatedly;
+kelembaban = analogRead(pin);
+//kelembaban = map(kelembaban, 0, 1024, 100, 0);
+Serial.println(kelembaban);
+
+if (kelembaban > 600) {
+  digitalWrite(pompa, HIGH)
+  }
+else {
+  digitalWrite(pompa, LOW)
+  }
+}
